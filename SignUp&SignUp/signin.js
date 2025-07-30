09ffd7db647af5fed714b4d3b5bee7698002af39
Vaibhav <vaibhav.chavda@digiqt.com>
2025-07-30 15:39:44 +0530

    myntra

--- SignUp&SignUp/signin.js
+++ SignUp&SignUp/signin.js
@@ -1,10 +1,35 @@
-document.getElementById("btn-submit").addEventListener("click", function () {
+document.getElementById("btn-submit").addEventListener("click", function (event) {
+    event.preventDefault();
+
+
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if (email === "" || password === "") {
-        alert('Please fill the field');
-    } else {
-        alert('SignIn Successful');
+
+    document.getElementById('error-email').innerHTML = '';
+    document.getElementById('error-password').innerHTML = '';
+
+
+    if ( email === '' && password === '') {
+        const text = 'Please fill all the fields';
+        document.getElementById('error-email').innerHTML = text;
+        document.getElementById('error-password').innerHTML = text;
+        return;
+    }
+
+    let isValid = true;
+
+    if (email === '') {
+        document.getElementById('error-email').innerHTML = 'Please enter email';
+        isValid = false;
+    }
+
+    if (password === '') {
+        document.getElementById('error-password').innerHTML = 'Please enter password';
+        isValid = false;
+    }
+
+    if (isValid) {
+        window.location.href = "signin.html";
     }
 });
--- SignUp&SignUp/signup.js
+++ SignUp&SignUp/signup.js
@@ -1,14 +1,51 @@
 document.getElementById("btn-submit").addEventListener("click", function (event) {
     event.preventDefault();
 
+
     const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const contact = document.getElementById("contact").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if (username && email && contact && password) {
+
+    document.getElementById('error-name').innerHTML = '';
+    document.getElementById('error-email').innerHTML = '';
+    document.getElementById('error-contact').innerHTML = '';
+    document.getElementById('error-password').innerHTML = '';
+
+
+    if (username === '' && email === '' && contact === '' && password === '') {
+        const text = 'Please fill all the fields';
+        document.getElementById('error-name').innerHTML = text;
+        document.getElementById('error-email').innerHTML = text;
+        document.getElementById('error-contact').innerHTML = text;
+        document.getElementById('error-password').innerHTML = text;
+        return;
+    }
+
+    let isValid = true;
+
+    if (username === '') {
+        document.getElementById('error-name').innerHTML = 'Please enter name';
+        isValid = false;
+    }
+
+    if (email === '') {
+        document.getElementById('error-email').innerHTML = 'Please enter email';
+        isValid = false;
+    }
+
+    if (contact === '') {
+        document.getElementById('error-contact').innerHTML = 'Please enter contact no';
+        isValid = false;
+    }
+
+    if (password === '') {
+        document.getElementById('error-password').innerHTML = 'Please enter password';
+        isValid = false;
+    }
+
+    if (isValid) {
         window.location.href = "signin.html";
-    } else {
-        alert("Please fill  the fields.");
     }
 });
