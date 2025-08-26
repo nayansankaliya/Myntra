d231ce20c40c2811e324534ffe5cc78794ec22a5
Vaibhav <vaibhav.chavda@digiqt.com>
2025-08-26 14:24:45 +0530

    myntra

--- scripts/forgotpass.js
+++ scripts/forgotpass.js
@@ -1,33 +1,33 @@
 document.getElementById("btn-submit").addEventListener("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.getElementById("email").value.trim().toLowerCase();
+  const email = document.getElementById("email").value.trim().toLowerCase();
 
-    document.getElementById('error-email').innerHTML = '';
-
-
-    if (email === '') {
-        const errorMsg = 'Please enter email';
-        document.getElementById('error-email').innerHTML = errorMsg;
-        return;
-    }
-
-    let isValid = true;
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email === '') {
-        document.getElementById('error-email').innerHTML = 'Please enter email';
-        isValid = false;
-    } else if (!emailRegex.test(email)) {
-        document.getElementById('error-email').innerHTML = 'Enter a valid email address';
-        isValid = false;
-    }
+  document.getElementById('error-email').innerHTML = '';
 
 
-    if (isValid) {
-        window.location.href = "../pages/newpass.html";
-    }
+  if (email === '') {
+    const errorMsg = 'Please enter email';
+    document.getElementById('error-email').innerHTML = errorMsg;
+    return;
+  }
+
+  let isValid = true;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email === '') {
+    document.getElementById('error-email').innerHTML = 'Please enter email';
+    isValid = false;
+  } else if (!emailRegex.test(email)) {
+    document.getElementById('error-email').innerHTML = 'Enter a valid email address';
+    isValid = false;
+  }
+
+  if (isValid) {
+    window.location.href = "../pages/newpass.html";
+  }
 });
+
 const hamburgerBtn = document.getElementById('hamburgerBtn');
 const navBar = document.getElementById('main-nav');
 const actionBar = document.querySelector('.action_bar');
@@ -35,10 +35,10 @@ const forgotpassContainer = document.querySelector('.forgotpass-container');
 
 hamburgerBtn.addEventListener('click', () => {
   navBar.classList.toggle('show');
-  actionBar.classList.toggle('show'); 
+  actionBar.classList.toggle('show');
 
   if (forgotpassContainer.style.marginTop === '430px') {
-    forgotpassContainer.style.marginTop = '0px'; 
+    forgotpassContainer.style.marginTop = '0px';
   } else {
     forgotpassContainer.style.marginTop = '430px';
   }
