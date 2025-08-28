0109070fdbc287f2149b761d638ec81a661b3cb4
Vaibhav <vaibhav.chavda@digiqt.com>
2025-08-28 13:10:27 +0530

    myntra

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,4 +1,20 @@
-document.getElementById("profileForm").addEventListener("submit", function(e) {
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const navBar = document.getElementById('main-nav');
+const actionBar = document.querySelector('.action_bar');
+const editContainer = document.querySelector('.container');
+
+hamburgerBtn.addEventListener('click', () => {
+  navBar.classList.toggle('show');
+  actionBar.classList.toggle('show');
+
+  if (editContainer.style.marginTop === '430px') {
+    editContainer.style.marginTop = '60px';
+  } else {
+    editContainer.style.marginTop = '430px';
+  }
+});
+
+document.getElementById("profileForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Profile details saved successfully!");
 });
